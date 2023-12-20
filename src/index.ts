38b0c3303cdc877f3844fa2ec3fa00080b3c001c
HuuSon <sonnh3@vmogroup.com>
2023-12-20 17:37:00 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,24 @@ type Args = {
   perf: number;
 };
 
+
+let config = {} as any;
+// process.on("exit", async () => {
+//   console.log("🚀 ~ file: index.ts:15 ~ config:", config)
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
+//   await main(config);
+// });
+
 program
   .command("process")
   .requiredOption("--config <config>", "Path to a yaml config file")
   .action(async (args: Args) => {
-    await main(args);
+    const { config: configPath } = args
+    config = Config.from_yaml_file(configPath);
+    await main(config);
   });
 
-async function main({ config: configPath }: Args) {
-  const config = Config.from_yaml_file(configPath);
+async function main(config: any) {
   const processor = new EventProcessor();
   const worker = new Worker({
     config,
